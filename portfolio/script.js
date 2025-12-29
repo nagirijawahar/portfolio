137726af667c8ae176ef5e95cc6bafe8b9aa0f3d
nagirijawahar <nagirijawaharreddi@gmail.com>
2025-12-29 22:29:48 +0530

    Delete portfolio directory

--- portfolio/script.js
+++
@@ -1,8 +0,0 @@
-// Smooth scrolling for navigation links
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href'))
-            .scrollIntoView({ behavior: 'smooth' });
-    });
-});
